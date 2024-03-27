aaad12fbaf63e6a88cd58527ff40390d4528555c
Roman Grachev <romagra92@gmail.com>
2024-03-27 15:12:32 +0500

    added an adaptive tablet layout

--- src/pages/page404/Page404.tsx
+++ src/pages/page404/Page404.tsx
@@ -5,13 +5,16 @@ import { Link } from 'react-router-dom';
 import styles from './page404.module.scss';
 
 import img from './404.gif';
+import classNames from 'classnames';
 
 export const Page404: FC = () => {
   return (
     <div className={styles.error}>
-      <img className={styles.error__image} src={img} alt='Ошибка 404' />
-      <p className={styles.error__text}>К сожалению, этой страницы не существует</p>
-      <Link to={'/'} className={styles.error__link}>Вернуться на главную страницу</Link>
+      <div className={classNames('container', styles.error__container)}>
+        <img className={styles.error__image} src={img} alt='Ошибка 404' />
+        <p className={styles.error__text}>К сожалению, этой страницы не существует</p>
+        <Link to={'/'} className={styles.error__link}>Вернуться на главную страницу</Link>
+      </div>
     </div>
   );
 };
